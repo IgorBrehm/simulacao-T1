88a64ac98699ae341a51191b1dc811ed75748ef9
IgorBrehm <igorbrehm@hotmail.com>
2020-03-14 18:31:25 -0300

    criada classe main

---
+++ Simulator.java
@@ -0,0 +1,12 @@
+public class Simulator{
+    public static void main(String args[]){
+
+        // ler arquivo de entrada e criar/popular objetos
+
+        Task firstArrival = new Task(2, "F1", "arrival");
+
+        for(int iterations = 0; iterations < 10; iterations++){
+
+        }
+    }
+}
