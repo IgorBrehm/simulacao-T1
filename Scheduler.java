28a7b0a8161df907c3aae3cbae84bb9042bd5691
IgorBrehm <igorbrehm@hotmail.com>
2020-03-14 18:30:56 -0300

    criado objeto escalonador

---
+++ Scheduler.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Scheduler{
+    public static PriorityQueue<Task> schedulerQueue = new PriorityQueue<Task>(new taskComparator());
+}
+
+class taskComparator implements Comparator<Task>{ 
+              
+    public int compare(Task t1, Task t2) { 
+        if (t1.getScheduledTime() < t2.getScheduledTime()){
+            return 1; 
+        }
+        else if (t1.getScheduledTime() >= t2.getScheduledTime()){ 
+            return -1; 
+        }
+        else{
+            return 0;
+        }
+    }
+}
